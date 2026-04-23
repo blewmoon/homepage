f30baaa7a0846ae8f0512abb0186559f2c65580f
rin <116615302+blewmoon@users.noreply.github.com>
2026-04-23 19:08:54 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+// dropdowns
+
+function guestbook() {
+  var x = document.getElementById("gb");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function aboutme() {
+  var x = document.getElementById("aboutme");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function blog() {
+  var x = document.getElementById("blog");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function ext() {
+  var x = document.getElementById("ext");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
